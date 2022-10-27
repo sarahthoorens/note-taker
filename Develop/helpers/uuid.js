0148dcd384e957d488f4887373c8a18e8385dc9b
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-27 00:28:33 -0500

    added code to handle viewing old notes and post request. incomplete.

---
+++ Develop/helpers/uuid.js
@@ -0,0 +1,3 @@
+const uuid  = require('uuid')
+// console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
+const noteId = uuid.v4()
--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,18 @@
 const express = require('express');
+const app = express();
+const fs = require('fs');
 const path = require('path');
 const savedNoteData = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
-const app = express();
+
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/assets/index.html') 
-// );
-// console.log('should be index')}
-// );
-
-
 
 // `GET /notes` returns the `notes.html` file.
 app.get('/notes', (req, res) => {
@@ -26,8 +21,18 @@ app.get('/notes', (req, res) => {
 
 // "`GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    res.json(savedNoteData)
-});
+    // Obtain existing note
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNote = JSON.parse(data);
+       
+    }   return res.status(201).json(parsedNote) ;
+    })
+  });
+       
 
 app.get('/api/notes/:term', (req, res) => {
     const noteRequested = req.params.term.toLowerCase();
@@ -42,26 +47,46 @@ app.get('/api/notes/:term', (req, res) => {
 
 //* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
+  	
+console.log(req.body);
     // Log that a POST request was received
-    console.info(`${req.method} request received to add a new note`);
-  
-    // Prepare a response object to send back to the client
-    let response;
-  
+      console.info(`${req.method} request received to add a new note`);
+   
+    const { title, text } = req.body;
+
     // Check if there is anything in the response body
-    if (req.body && req.body.title && req.body.text) {
-      response = {
-        status: 'success',
-        data: req.body,
+    if (title && text) {
+
+      const newNote = {
+        title,
+        text,
+        note_id: uuid,
       };
-      res.json(`New Note has been added!`);
-    } else {
-      res.json('Note must contain title and text.');
-    }
+  
+        // Add a new note
+        parsedNote.push(newNote);
+
+        // Write updated note back to the file
+        fs.writeFile('./db/db.json',JSON.stringify(parsedNote, null, 4),
+          (writeErr) => writeErr ? console.error(writeErr)
+              : console.info('Successfully added new note!')
+        );
+      }
+    });
+
+      const response = {
+        status: 'success',
+        // body: newNote,
+      };
+      console.log(response);
+    //   res.status(201).json(response);
+    //  else {
+    //   res.status(500).json('Note must contain title and text.');
+    // }
   
     // Log the response body to the console
     console.log(req.body);
-  });
+
   
   // Return the `index.html` file.
 app.get('*', (req, res) => {

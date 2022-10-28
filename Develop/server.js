0d423640d40adabd5d41bb7bc377cd9a9f251119
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-28 02:05:58 -0500

    added a delete function that works occasionally.

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+let db = require('./db/db.json')
 const uuid = require('./helpers/uuid');
 const PORT = 3001;
+
 
 const app = express();
 
@@ -19,24 +21,23 @@ app.get('/', (req, res) => {
 });
 // `GET /notes` returns the `notes.html` file.
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 
 // `GET /api/notes` reads the `db.json` file and returns all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-  console.log('is this working?')
   console.info(`${req.method} request received.`);
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {    
-        const allNotes=JSON.parse(data);
-        res.json(allNotes);
-      }   
-    })
-  });
+  // Obtain existing notes
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const allNotes = JSON.parse(data);
+      res.json(allNotes);
+    }
+  })
+});
 
 
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
@@ -44,7 +45,7 @@ app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note.`);
 
-    // Destructuring assignment for the items in req.body
+  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
   // Check if there is anything in the response body
@@ -55,24 +56,24 @@ app.post('/api/notes', (req, res) => {
       id: uuid(),
     };
     // Obtain existing notes
-       fs.readFile('./db/db.json', 'utf8', (err, note) => {
-          if (err) {
-            console.error(err);
-          } else {
-          const parsedNotes = JSON.parse(note);
-          parsedNotes.push(newNote)
-          
+    fs.readFile('./db/db.json', 'utf8', (err, note) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(note);
+        parsedNotes.push(newNote)
 
-              // Write updated note back to the file
-          fs.writeFile('./db/db.json',JSON.stringify(parsedNotes, null, 4),
+
+        // Write updated note back to the file
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
           (writeErr) => writeErr ? console.error(writeErr)
-          : console.info('Successfully added new note!')
-          );
-        }
-      });
-          const response = {
-              status: 'success',
-              body: newNote,
+            : console.info('Successfully added new note!')
+        );
+      }
+    });
+    const response = {
+      status: 'success',
+      body: newNote,
     };
     console.log(response);
     res.status(201).json(response);
@@ -87,7 +88,44 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-  app.listen(PORT, () =>
-  console.log(`Express server listening on port http://localhost:${PORT}`)
-);
+// Delete notes - this code works on occasion - needs debugging
+app.delete('/api/notes/:id', (req, res) => {
+      console.info(`${req.method} request received to delete note.`);
+      for (let i = 0; i < db.length; i++) {
+        let currentNote = db[i];
+        console.log(currentNote);
+        if (currentNote.id === req.params.id) {
+          let newList = db.splice([i],1);
+          res.status(200).json(`The note has been deleted`);
+          console.log(db);
+          fs.writeFile('./db/db.json', JSON.stringify(newList), (err) =>
+          err ? console.error(err) : console.log('Note list has been updated.')
+          )}
+      } 
+    });
 
+    app.listen(PORT, () =>
+      console.log(`Express server listening on port http://localhost:${PORT}`)
+    );
+
+    // this code needs debugging
+    // app.delete('/api/notes/:id', (req, res) => {
+    //   //   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      //     if (err) {
+      //       console.error(err);
+      //     } else {
+      //       const allNotes = JSON.parse(data);
+      //      for (let i = 0; i < allNotes.length; i++) {
+      //         console.log(allNotes);
+      //           if (allNotes.id === req.params.id) {
+      //             allNotes.splice([i], 1);
+      //             fs.writeFile('./db/db.json', JSON.stringify(allNotes), (err) =>
+      //             err ? console.error(err) : console.log('Note list has been updated.'));
+      //             res.status(201).json('Note list updated.');
+      //       }
+      //           else {
+      //             console.log('Unable to delete the note.')
+      //             break
+      //       }
+      //       }} 
+      // })});

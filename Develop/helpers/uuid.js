8cbbbfda92055dfda15d76e838d43ec64d73f6d0
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-27 22:00:15 -0500

    appends to json db file but does not render to page yet

--- Develop/helpers/uuid.js
+++ Develop/helpers/uuid.js
@@ -1,3 +1,3 @@
 const uuid  = require('uuid')
 // console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
-const noteId = uuid.v4()
+module.exports = () => uuid.v4()
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
 const path = require('path');
-const savedNoteData = require('./db/db.json');
+const fs = require('fs');
 const uuid = require('./helpers/uuid');
-
 const PORT = 3001;
+
+const app = express();
 
 
 // Middleware for parsing application/json and urlencoded data
@@ -14,84 +13,84 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// `GET /notes` returns the `notes.html` file.
-app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/public/notes.html');
+// `GET /` returns the `index.html` file.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// "`GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+// `GET /notes` reads the `db.json` file and returns all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    // Obtain existing note
+  console.log('is this working?')
+  console.info(`${req.method} request received.`);
+    // Obtain existing notes
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
-      } else {
-        // Convert string into JSON object
-        const parsedNote = JSON.parse(data);
-       
-    }   return res.status(201).json(parsedNote) ;
+      } else {    
+           
+               // Add a new review
+               return res.status(200).json(data);
+      
+    }   
     })
   });
-       
 
-app.get('/api/notes/:term', (req, res) => {
-    const noteRequested = req.params.term.toLowerCase();
-
-    for (let i = 0; i < savedNoteData.length; i++) {
-        if (noteRequested === savedNoteData[i].title.toLowerCase()) {
-            return res.json(savedNoteData[i]);
-        }
-    }
-    return res.json('No note found')
+// `GET /notes` returns the `notes.html` file.
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
-  	
-console.log(req.body);
-    // Log that a POST request was received
-      console.info(`${req.method} request received to add a new note`);
-   
-    const { title, text } = req.body;
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note.`);
 
-    // Check if there is anything in the response body
-    if (title && text) {
+    // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-      const newNote = {
-        title,
-        text,
-        note_id: uuid,
-      };
-  
-        // Add a new note
-        parsedNote.push(newNote);
+  // Check if there is anything in the response body
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    // Obtain existing notes
+       fs.readFile('./db/db.json', 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+          } else {
+    // Convert string into JSON object
+    const parsedNote= JSON.parse(data);
 
-        // Write updated note back to the file
-        fs.writeFile('./db/db.json',JSON.stringify(parsedNote, null, 4),
+    // Add a new review
+    parsedNote.push(newNote);
+
+              // Write updated note back to the file
+          fs.writeFile('./db/db.json',JSON.stringify(parsedNote, null, 4),
           (writeErr) => writeErr ? console.error(writeErr)
-              : console.info('Successfully added new note!')
-        );
-      }
-    });
-
-      const response = {
-        status: 'success',
-        // body: newNote,
-      };
-      console.log(response);
-    //   res.status(201).json(response);
-    //  else {
-    //   res.status(500).json('Note must contain title and text.');
-    // }
-  
-    // Log the response body to the console
-    console.log(req.body);
-
-  
-  // Return the `index.html` file.
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+          : console.info('Successfully added new note!'));
+        }
+      });
+          const response = {
+              status: 'success',
+              body: newNote
+    };
+    console.log(response);
+    res.status(201).json(response);
+  }
+  else {
+    res.status(500).json('Note must contain title and text.')
+  }
 });
+
+//  Return the `index.html` file for random queries.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
   app.listen(PORT, () =>
   console.log(`Express server listening on port http://localhost:${PORT}`)
 );
+
